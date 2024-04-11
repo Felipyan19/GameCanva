f171d13724ac1139e58ede8df57cda61929cc753
Andres Castaño <felipyan199@gmail.com>
2024-04-11 15:49:16 -0500

    fix: camera. AC

--- src/components/Photo/index.jsx
+++ src/components/Photo/index.jsx
@@ -4,22 +4,26 @@ import Webcam from 'react-webcam';
 function Photo() {
   const webcamRef = useRef(null);
   const [capturedPhoto, setCapturedPhoto] = useState(null);
+  const [cameraOn, setCameraOn] = useState(true);
 
   const capture = () => {
     const imageSrc = webcamRef.current.getScreenshot();
     setCapturedPhoto(imageSrc);
     localStorage.setItem('capturedPhoto', imageSrc);
+    setCameraOn(false); // Apagar la cámara después de capturar la foto
   };
 
   return (
     <div>
-      <Webcam
-        audio={false}
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-        style={{ width: '100%', maxWidth: '500px' }}
-      />
-      <button onClick={capture}>Capturar</button>
+      {cameraOn && (
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          style={{ width: '100%', maxWidth: '500px' }}
+        />
+      )}
+      {cameraOn && <button onClick={capture}>Capturar</button>}
       {capturedPhoto && <img src={capturedPhoto} alt="Captured" />}
     </div>
   );
