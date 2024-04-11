1f1fc856f4cb55dacc1257a99594769440523492
Andres Castaño <felipyan199@gmail.com>
2024-04-11 14:58:00 -0500

    Feature: page register dif. AC

--- src/components/Photo/index.jsx
+++ src/components/Photo/index.jsx
@@ -1,38 +1,25 @@
 import React, { useRef, useState } from 'react';
+import Webcam from 'react-webcam';
 
 function Photo() {
-  const videoRef = useRef(null);
+  const webcamRef = useRef(null);
   const [capturedPhoto, setCapturedPhoto] = useState(null);
 
-  const handleCapture = () => {
-    const video = videoRef.current;
-
-    if (video && video.srcObject) {
-      const canvas = document.createElement('canvas');
-      const context = canvas.getContext('2d');
-
-      // Configurar el tamaño del lienzo para que coincida con el tamaño del video
-      canvas.width = video.videoWidth;
-      canvas.height = video.videoHeight;
-
-      // Dibujar el fotograma actual del video en el lienzo
-      context.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-      // Convertir la imagen del lienzo a base64
-      const imageDataURL = canvas.toDataURL('image/jpeg');
-
-      // Guardar la imagen en el localStorage
-      localStorage.setItem('capturedPhoto', imageDataURL);
-
-      // Actualizar el estado para mostrar la imagen capturada
-      setCapturedPhoto(imageDataURL);
-    }
+  const capture = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setCapturedPhoto(imageSrc);
+    localStorage.setItem('capturedPhoto', imageSrc);
   };
 
   return (
     <div>
-      <video ref={videoRef} autoPlay />
-      <button onClick={handleCapture}>Capturar</button>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        style={{ width: '100%', maxWidth: '500px' }}
+      />
+      <button onClick={capture}>Capturar</button>
       {capturedPhoto && <img src={capturedPhoto} alt="Captured" />}
     </div>
   );
